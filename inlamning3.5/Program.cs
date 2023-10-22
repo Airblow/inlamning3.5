8c6d72def62c6bf67d0e22c327cfd985148aa9b9
Airblow <143172177+Airblow@users.noreply.github.com>
2023-10-22 17:27:11 +0200

    Add project files.

---
+++ inlamning3.5/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+namespace inlamning3._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in två tal");
+            string answer = Console.ReadLine();
+            int space = answer.IndexOf(" ");
+            int number1 = int.Parse(answer[..space]);
+            int number2 = int.Parse(answer[space..]);
+
+            Console.WriteLine("Välj ett räknesätt");
+            Console.WriteLine("1. Addition");
+            Console.WriteLine("2. Subtraktion");
+            Console.WriteLine("3. Multiplikation");
+            Console.WriteLine("4. Division");
+            string method = Console.ReadLine();
+
+            switch (method)
+            {
+                case "1":
+                    int addition = number1 + number2;
+                    Console.WriteLine($"{number1} plus {number2} blir {addition}");
+                    break;
+
+                case "2":
+                    int subtraction = number1 - number2;
+                    Console.WriteLine($"{number1} minus {number2} blir {subtraction}");
+                    break;
+
+                case "3":
+                    int multiplication = number1 * number2;
+                    Console.WriteLine($"{number1} gånger {number2} blir {multiplication}");
+                    break;
+
+                case "4":
+                    double division = (double)number1 / (double)number2;
+                    Console.WriteLine($"{number1} dividerat med {number2} blir {division}");
+                    break;
+
+            }
+            Console.ReadKey();  
+        
+        }
+
+
+    }
+
+}
